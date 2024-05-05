8d67ccb55935c0506cf5aab97e8c0e24ffc49d24
Leonardo Bustamante <leobusta@gmail.com>
2024-05-05 13:29:47 -0500

    clase intro next

---
+++ app/(general)/about/page.tsx
@@ -0,0 +1,8 @@
+
+export default function About() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-20">
+    <h1 className="flex items-center">About</h1>
+    </main>
+  )
+}
---
+++ app/(general)/contact/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Contact() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-20">
+    <h1 className="flex items-center">Contact</h1>
+    </main>
+  )
+}
